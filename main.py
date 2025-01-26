e6ebc439cab11cd494943ac1e09c01d17b66a8ba
Domroon <haeusser-dominik@web.de>
2025-01-26 13:27:47 +0100

    split big matrix class into seperate classes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from asyncio import sleep, create_task, run, get_event_loop
-from matrix import Matrix
+from screen import TimeScreen, Matrix
 
 
 async def print_hello_1():
@@ -14,20 +14,21 @@ async def print_hello_2():
         await sleep(1)
 
 
-async def show_time():
-    matrix = Matrix()
-    matrix.clear()
+async def show_time(matrix):
+    time_screen = TimeScreen(matrix)
+    time_screen.matrix.clear()
     minutes = 0
     while True:
-        matrix.show_time(2, minutes)
+        time_screen.show_time(2, minutes)
         minutes = minutes + 1
         await sleep(1)
 
 
 async def main():
+    matrix = Matrix()
     create_task(print_hello_1())
     create_task(print_hello_2())
-    create_task(show_time())
+    create_task(show_time(matrix))
 
     event_loop = get_event_loop()
     event_loop.run_forever()
