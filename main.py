8231289156224259654dffb75560264fc09dcb10
domroon <d.duehr@web.de>
2025-02-12 19:09:06 +0100

    implemented  the first states of UpdateStateMachine

--- main.py
+++ main.py
@@ -1,50 +1,133 @@
 from machine import RTC
 import network
-from asyncio import sleep, create_task, run, get_event_loop
+from asyncio import sleep, create_task, run, get_event_loop, Event
 
+from ds3231 import Timekeeper
 from config import config
 from screen import TimeScreen, Matrix, AnimationScreen, BLUE
 import events
 
 
+# Network Events
+
+wlan_connected = Event()
+wlan_connected_timeout = Event()
+
+# Update StateMachine Events - es kann immer nur ein StateMachine Event aktiv sein, nie mehrere gleichzeitig (außer booted)
+
+booted = Event()                # 0 - hier kann unterschieden werden ob das System schon an war oder neu gebootet ist
+checkTimekeeperState = Event()  # 1 - Hier wird geprüft ob der Timekeeper eine valide Uhrzeit liefert
+connectToWlan = Event()         # 2 - Wird gesetzt wenn gerade versucht wird eine Internetverbindung aufzubauen
+checkForUpdates = Event()       # 3 - Es wird gerade nach Updates gesucht
+deliverWebServer = Event()      # 4 - Der Webserver zum eingeben von SSID und Password fürs Wlan ist gerade aktiv
+updateFirmware = Event()        # 5 - Die Firmware wird gerade aktualisiert
+waitToCheck = Event()           # 6 - Es wird gerade gewartet um später nach Updates zu suchen
+
+# Time Validation Events
+
+time_set_by_internet = Event()
+timekeeper_time_is_valid = Event()
+
+# Screen Events - es kann immer nur ein Screen Event aktiv sein, nie mehrere gleichzeitig
+
+showStartInfo = Event()         # Sollte einmal nach dem Start aktiv sein um zu zeigen ob der Timekeeper eine valide Zeit hat,
+                                # ob Wlan verbunden ist und ob auf Updates geprüft werden konnte
+                                # Erste LED zeigt grün oder rot (je nachdem ob Timekeeper valide ist)
+                                # Zweite LED zeigt grün oder rot (je nachdem ob Internet verbunden)
+                                # Dritte LED zeigt grün für Updates überprüft, es liegen keine neuen vor
+                                # rot für es konnte nicht auf neue Updates geprüft werden und gelb für 
+                                # es liegen neue Updates vor
+showTime = Event()              # Sollte immer dann gesetzt werden wenn die Zeit angezeigt werden soll
+showUpdateProgress = Event()    # Sollte angezeigt werden wenn gerade ein Update durchgeführt wird
+
+
 class UpdateStateMachine:
-    def __init__(self):
+    def __init__(self, timekeeper, wlan):
         self.state = 0
+        self.timekeeper: Timekeeper = timekeeper
+        self.wlan: network.WLAN = wlan
     
     async def boot_device(self):
         # state 0
-        pass
+        booted.set()
+        self.state = 1
 
     async def check_timekeeper_state(self):
         # state 1
-        pass
-
+        checkTimekeeperState.set()
+        if not(self.timekeeper.is_time_lost()):
+            timekeeper_time_is_valid.set()
+        else:
+            timekeeper_time_is_valid.clear()
+        self.state = 2
+        checkTimekeeperState.clear()
+        
     async def connect_to_wlan(self):
         # state 2
-        pass
+        connectToWlan.set()
+        connect_to_wlan(self.wlan)
+        while True:
+            if wlan_connected_timeout.is_set() and booted.is_set():
+                self.state = 4
+                break
+            if wlan_connected_timeout.is_set():
+                self.state = 6
+                break
+            if wlan_connected.is_set():
+                self.state = 3
+                break
+            await sleep(1)
+        connectToWlan.clear()
 
     async def check_for_updates(self):
         # state 3
-        pass
+        checkForUpdates.set()
+        # check udpdates here
+        await sleep(5)
+        checkForUpdates.clear()
 
     async def deliver_webserver(self):
         # state 4
-        pass
+        deliverWebServer.set()
+        # deliver webserver here
+        await sleep(5)
+        deliverWebServer.clear()
 
     async def update_firmware(self):
         # state 5
-        pass
+        updateFirmware.set()
+        # update firmware here
+        await sleep(5)
+        updateFirmware.clear()
 
     async def wait_to_check_updates(self):
         # state 6
-        pass
+        waitToCheck.set()
+        # wait here
+        await sleep(5)
+        waitToCheck.clear()
 
     async def set_current_event(self):
         pass
 
     async def start(self):
         while True:
-            pass
+            print('UpdateStateMachine in state: ', self.state)
+            if self.state == 0:
+                create_task(self.boot_device())
+            elif self.state == 1:
+                create_task(self.check_timekeeper_state())
+            elif self.state == 2:
+                create_task(self.connect_to_wlan())
+            elif self.state == 3:
+                create_task(self.check_for_updates())
+            elif self.state == 4:
+                create_task(self.deliver_webserver())
+            elif self.state == 5:
+                create_task(self.update_firmware())
+            elif self.state == 6:
+                create_task(self.wait_to_check_updates())
+            await sleep(1)
 
 # general coroutines and functions
 
@@ -136,23 +219,23 @@ def connect_to_wlan(wlan):
 async def set_wlan_connected_event(wlan):
     while True:
         if wlan.isconnected():
-            events.wlan_connected.set()
+            wlan_connected.set()
         else:
-            events.wlan_connected.clear()
+            wlan_connected.clear()
         await sleep(5)
 
 async def set_wlan_connected_timeout_event():
-    events.wlan_connected_timeout.clear()
+    wlan_connected_timeout.clear()
     timer = 0
     max_time = 10
     while True:
-        if events.wlan_connected.is_set():
-            events.wlan_connected_timeout.clear()
+        if wlan_connected.is_set():
+            wlan_connected_timeout.clear()
             timer = 0
         if timer >= max_time:
-            events.wlan_connected_timeout.set()
+            wlan_connected_timeout.set()
             await sleep(5)
-            events.wlan_connected_timeout.clear()
+            wlan_connected_timeout.clear()
             timer = 0
         timer = timer + 1
         await sleep(1)
@@ -165,9 +248,13 @@ async def set_wlan_connected_timeout_event():
 # print("test")
 
 async def main():
+    rtc = RTC() 
     matrix = Matrix()
     animation_screen = AnimationScreen(matrix)
     wlan = network.WLAN(network.WLAN.IF_STA)
+    timekeeper = Timekeeper(rtc)
+
+    update_state_machine = UpdateStateMachine(timekeeper, wlan)
 
     matrix.clear()
 
@@ -180,9 +267,12 @@ async def main():
     create_task(set_wlan_connected_timeout_event())
     create_task(set_wlan_connected_event(wlan))
 
+    # state machine tasks
+    create_task(update_state_machine.start())
+
     # show tasks
-    create_task(show_wait_animation(animation_screen))
-    create_task(show_network_connection_status(animation_screen))
+    # create_task(show_wait_animation(animation_screen))
+    # create_task(show_network_connection_status(animation_screen))
 
     event_loop = get_event_loop()
     event_loop.run_forever()
